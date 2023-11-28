909b990df357dc73b50f1c2ab4cc521ee0790cab
ErickVGusson <92764323+ErickVGusson@users.noreply.github.com>
2023-11-28 11:50:20 -0300

    Quase completo

--- WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -28,13 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rbAlternativaCorretaA = new System.Windows.Forms.RadioButton();
+            this.rtxPergunta = new System.Windows.Forms.RichTextBox();
+            this.txbAlternativaA = new System.Windows.Forms.TextBox();
+            this.txbAlternativaB = new System.Windows.Forms.TextBox();
+            this.rbAlternativaCorretaB = new System.Windows.Forms.RadioButton();
+            this.txbAlternativaC = new System.Windows.Forms.TextBox();
+            this.rbAlternativaCorretaC = new System.Windows.Forms.RadioButton();
+            this.txbAlternativaD = new System.Windows.Forms.TextBox();
+            this.rbAlternativaCorretaD = new System.Windows.Forms.RadioButton();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rbAlternativaCorretaA
+            // 
+            this.rbAlternativaCorretaA.AutoSize = true;
+            this.rbAlternativaCorretaA.Location = new System.Drawing.Point(12, 121);
+            this.rbAlternativaCorretaA.Name = "rbAlternativaCorretaA";
+            this.rbAlternativaCorretaA.Size = new System.Drawing.Size(32, 17);
+            this.rbAlternativaCorretaA.TabIndex = 0;
+            this.rbAlternativaCorretaA.TabStop = true;
+            this.rbAlternativaCorretaA.Text = "A";
+            this.rbAlternativaCorretaA.UseVisualStyleBackColor = true;
+            // 
+            // rtxPergunta
+            // 
+            this.rtxPergunta.Location = new System.Drawing.Point(12, 12);
+            this.rtxPergunta.Name = "rtxPergunta";
+            this.rtxPergunta.Size = new System.Drawing.Size(270, 100);
+            this.rtxPergunta.TabIndex = 1;
+            this.rtxPergunta.Text = "";
+            // 
+            // txbAlternativaA
+            // 
+            this.txbAlternativaA.Location = new System.Drawing.Point(50, 118);
+            this.txbAlternativaA.Name = "txbAlternativaA";
+            this.txbAlternativaA.Size = new System.Drawing.Size(230, 20);
+            this.txbAlternativaA.TabIndex = 2;
+            // 
+            // txbAlternativaB
+            // 
+            this.txbAlternativaB.Location = new System.Drawing.Point(50, 144);
+            this.txbAlternativaB.Name = "txbAlternativaB";
+            this.txbAlternativaB.Size = new System.Drawing.Size(230, 20);
+            this.txbAlternativaB.TabIndex = 4;
+            // 
+            // rbAlternativaCorretaB
+            // 
+            this.rbAlternativaCorretaB.AutoSize = true;
+            this.rbAlternativaCorretaB.Location = new System.Drawing.Point(12, 147);
+            this.rbAlternativaCorretaB.Name = "rbAlternativaCorretaB";
+            this.rbAlternativaCorretaB.Size = new System.Drawing.Size(32, 17);
+            this.rbAlternativaCorretaB.TabIndex = 3;
+            this.rbAlternativaCorretaB.TabStop = true;
+            this.rbAlternativaCorretaB.Text = "B";
+            this.rbAlternativaCorretaB.UseVisualStyleBackColor = true;
+            // 
+            // txbAlternativaC
+            // 
+            this.txbAlternativaC.Location = new System.Drawing.Point(50, 170);
+            this.txbAlternativaC.Name = "txbAlternativaC";
+            this.txbAlternativaC.Size = new System.Drawing.Size(230, 20);
+            this.txbAlternativaC.TabIndex = 6;
+            // 
+            // rbAlternativaCorretaC
+            // 
+            this.rbAlternativaCorretaC.AutoSize = true;
+            this.rbAlternativaCorretaC.Location = new System.Drawing.Point(12, 173);
+            this.rbAlternativaCorretaC.Name = "rbAlternativaCorretaC";
+            this.rbAlternativaCorretaC.Size = new System.Drawing.Size(32, 17);
+            this.rbAlternativaCorretaC.TabIndex = 5;
+            this.rbAlternativaCorretaC.TabStop = true;
+            this.rbAlternativaCorretaC.Text = "C";
+            this.rbAlternativaCorretaC.UseVisualStyleBackColor = true;
+            // 
+            // txbAlternativaD
+            // 
+            this.txbAlternativaD.Location = new System.Drawing.Point(50, 196);
+            this.txbAlternativaD.Name = "txbAlternativaD";
+            this.txbAlternativaD.Size = new System.Drawing.Size(230, 20);
+            this.txbAlternativaD.TabIndex = 8;
+            // 
+            // rbAlternativaCorretaD
+            // 
+            this.rbAlternativaCorretaD.AutoSize = true;
+            this.rbAlternativaCorretaD.Location = new System.Drawing.Point(12, 199);
+            this.rbAlternativaCorretaD.Name = "rbAlternativaCorretaD";
+            this.rbAlternativaCorretaD.Size = new System.Drawing.Size(33, 17);
+            this.rbAlternativaCorretaD.TabIndex = 7;
+            this.rbAlternativaCorretaD.TabStop = true;
+            this.rbAlternativaCorretaD.Text = "D";
+            this.rbAlternativaCorretaD.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(12, 244);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(80, 29);
+            this.btnCadastrar.TabIndex = 9;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(291, 294);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.txbAlternativaD);
+            this.Controls.Add(this.rbAlternativaCorretaD);
+            this.Controls.Add(this.txbAlternativaC);
+            this.Controls.Add(this.rbAlternativaCorretaC);
+            this.Controls.Add(this.txbAlternativaB);
+            this.Controls.Add(this.rbAlternativaCorretaB);
+            this.Controls.Add(this.txbAlternativaA);
+            this.Controls.Add(this.rtxPergunta);
+            this.Controls.Add(this.rbAlternativaCorretaA);
+            this.Name = "Form1";
+            this.Text = "Cadastrar Perguntas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton rbAlternativaCorretaA;
+        private System.Windows.Forms.RichTextBox rtxPergunta;
+        private System.Windows.Forms.TextBox txbAlternativaA;
+        private System.Windows.Forms.TextBox txbAlternativaB;
+        private System.Windows.Forms.RadioButton rbAlternativaCorretaB;
+        private System.Windows.Forms.TextBox txbAlternativaC;
+        private System.Windows.Forms.RadioButton rbAlternativaCorretaC;
+        private System.Windows.Forms.TextBox txbAlternativaD;
+        private System.Windows.Forms.RadioButton rbAlternativaCorretaD;
+        private System.Windows.Forms.Button btnCadastrar;
     }
 }
 
--- WindowsFormsApp6/WindowsFormsApp6/Form1.cs
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApp6
 {
@@ -15,6 +16,82 @@ namespace WindowsFormsApp6
         public Form1()
         {
             InitializeComponent();
+        }
+
+        string conexaoString = "server=localhost;user=root;password=;database=db_quiz_p;";
+
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+
+            string pergunta = "";
+            string alternativaA = "", alternativaB = "", alternativaC = "", alternativaD = "";
+            long ultimoID = 0;
+
+
+            pergunta = rtxPergunta.Text;
+            alternativaA = txbAlternativaA.Text;
+            alternativaB = txbAlternativaB.Text;
+            alternativaC = txbAlternativaC.Text;
+            alternativaD = txbAlternativaD.Text;
+
+            using (MySqlConnection conexao = new MySqlConnection(conexaoString))
+            {
+
+                conexao.Open();
+                string scriptInsertQuestao = "INSERT INTO tb_perguntas (questao) VALUE (@questao)";
+
+                using (MySqlCommand comando = new MySqlCommand(scriptInsertQuestao, conexao))
+                {
+                    //Substitue os parametros para os valores reais
+                    comando.Parameters.AddWithValue("@questao", pergunta);
+
+                    comando.ExecuteNonQuery();
+
+                    ultimoID = comando.LastInsertedId;
+
+                }
+
+                string scriptInsertAlternativa = "INSERT INTO tb_alternativa (resposta, id_pergunta) VALUE (@resposta, @ultimoID)";
+
+                using (MySqlCommand comando = new MySqlCommand(scriptInsertAlternativa, conexao))
+                {
+                    //Substitue os parametros para os valores reais
+                    comando.Parameters.AddWithValue("@resposta", alternativaA);
+                    comando.Parameters.AddWithValue("@ultimoID", ultimoID);
+                    comando.ExecuteNonQuery();
+
+                }
+
+                using (MySqlCommand comando = new MySqlCommand(scriptInsertAlternativa, conexao))
+                {
+                    //Substitue os parametros para os valores reais
+                    comando.Parameters.AddWithValue("@resposta", alternativaB);
+                    comando.Parameters.AddWithValue("@ultimoID", ultimoID);
+                    comando.ExecuteNonQuery();
+
+                }
+
+                using (MySqlCommand comando = new MySqlCommand(scriptInsertAlternativa, conexao))
+                {
+                    //Substitue os parametros para os valores reais
+                    comando.Parameters.AddWithValue("@resposta", alternativaC);
+                    comando.Parameters.AddWithValue("@ultimoID", ultimoID);
+                    comando.ExecuteNonQuery();
+
+                }
+
+                using (MySqlCommand comando = new MySqlCommand(scriptInsertAlternativa, conexao))
+                {
+                    //Substitue os parametros para os valores reais
+                    comando.Parameters.AddWithValue("@resposta", alternativaD);
+                    comando.Parameters.AddWithValue("@ultimoID", ultimoID);
+                    comando.ExecuteNonQuery();
+
+                }
+
+            }
+            MessageBox.Show("Pergunta cadastrada com sucesso\n" + ultimoID.ToString());
         }
     }
 }
